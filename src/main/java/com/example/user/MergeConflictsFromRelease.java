e495eb9a42b253f61719ff8a42637eefd1382370
RajaRao <rajarao1284@gmail.com>
2023-08-15 16:10:31 +0530

    created class from release

---
+++ src/main/java/com/example/user/MergeConflictsFromRelease.java
@@ -0,0 +1,8 @@
+package com.example.user;
+
+public class MergeConflictsFromRelease {
+
+	public static void main(String[] args) {
+		System.out.println("Merge conflicts from release branch");
+	}
+}
